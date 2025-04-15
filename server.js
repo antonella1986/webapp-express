db376d85b502f480393caaf4d5093a7835d23434
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-15 16:18:07 +0200

    edit comments on server.js file

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const serverError = require('./middleware/serverError');
 const notFound = require('./middleware/notFound');
 
 //MIDDLEWARES
-//middleware cors, serve perché porcoddio
+//middleware cors, serve perché il client e il server sono in due domini differenti
 app.use(cors(
     {
         origin: process.env.FRONT_URL || 'http://localhost:5173'
     }
 ));
 
-//body parser (serve a parsare i dati che arrivano dal client cioè che porcoddio significa diocane)
+//body parser (serve a parsare i dati che arrivano dal client)
 app.use(express.json());
 
 //static assets (serve a rendere disponibili i file statici, come le immagini)
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
     res.send('Movies API server running');
 })
 
+//router (serve a gestire le richieste)
 app.use('/movies', moviesRouter);
 
 //middleware for server errors
